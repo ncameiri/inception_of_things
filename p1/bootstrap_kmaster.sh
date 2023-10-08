082e5638356dc879b171c04f7d8e0da19aecbf1d
Dmarceli <daniel.sequeira@skeyra.pt>
2023-10-08 16:12:16 +0100

    p1 done

--- p1/bootstrap_kmaster.sh
+++ p1/bootstrap_kmaster.sh
@@ -11,9 +11,8 @@ export token=$(echo $token | tr ":" "\n" | tail -1)
 
 echo "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=\"agent --node-ip 192.168.56.111  \"\
     K3S_URL=\"https://192.168.56.110:6443\"\
-    K3S_TOKEN=\"$token\"
-    K3S_KUBECONFIG_MODE=\"644\"
-      sh - " > joincluster.sh 
+    K3S_TOKEN=\"$token\"\
+    K3S_KUBECONFIG_MODE=\"644\" sh - " > joincluster.sh 
 
 
 
