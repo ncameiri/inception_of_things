e4a003a0b295654d1b41e042c526c141c6ff645c
Nuno Cameirinha <nunoocameirinha@gmail.com>
2023-10-08 15:47:44 +0100

    Updates

--- p1/bootstrap_kmaster.sh
+++ p1/bootstrap_kmaster.sh
@@ -3,16 +3,17 @@
 
 # Install k3s
 echo "Installing k3s..."
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --tls-san $(hostname)"\
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.56.110 --tls-san $(hostname) --advertise-address=192.168.56.110  --disable-network-policy --bind-address=192.168.56.110 "\
     K3S_KUBECONFIG_MODE="644" sh -
 
 export token=$(cat /var/lib/rancher/k3s/server/agent-token)
 export token=$(echo $token | tr ":" "\n" | tail -1)
 
-echo "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=\"agent --node-ip 192.168.56.111\"\
+echo "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC=\"agent --node-ip 192.168.56.111  \"\
     K3S_URL=\"https://192.168.56.110:6443\"\
-    K3S_TOKEN=\"$token\"\
-    K3S_KUBECONFIG_MODE=\"644\" sh - " > joincluster.sh 
+    K3S_TOKEN=\"$token\"
+    K3S_KUBECONFIG_MODE=\"644\"
+      sh - " > joincluster.sh 
 
 
 
